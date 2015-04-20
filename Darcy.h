173b123900c6c25aaea88be21ef8a798f7aff247
akva <akva@e10b68d5-8a6e-419e-a041-bce267b0401d>
2015-04-20 13:09:13 +0000

    added: doxy for darcy application
    git-svn-id: http://svn.sintef.no/trondheim/IFEM/trunk@3186 e10b68d5-8a6e-419e-a041-bce267b0401d

--- Darcy.h
+++ Darcy.h
@@ -20,120 +20,126 @@
 
 class DarcyNorm;
 
+/*!
+ *  \brief Class representing the integrand of the Darcy problem.
+ */
 class Darcy : public IntegrandBase
 {
-  public:
-    //! \brief Default constructor.
-    Darcy(unsigned short int n);
-    //! \brief Empty destructor.
-    virtual ~Darcy() {}
+public:
+  //! \brief Default constructor.
+  Darcy(unsigned short int n);
+  //! \brief Empty destructor.
+  virtual ~Darcy() {}
 
-    //void setMaterial(const Vec3& permeability) { perm = permeability; }
+  //! \brief Set permeability function.
+  void setPermValues(VecFunc* permv) { permvalues = permv; }
 
-    void setPermValues(VecFunc* permv) { permvalues = permv; }
+  //! \brief Set permeability field.
+  void setPermField(RealFunc* permf) { permeability = permf; }
 
-    void setPermField(RealFunc* permf) { permeability = permf; }
+  //! \brief Set body force vector.
+  void setBodyForce(VecFunc* body) { bodyforce = body; }
 
-    void setBodyForce(VecFunc* body) { bodyforce = body; }
+  //! \brief Defines the source function.
+  void setSource(RealFunc* src) { source = src; }
 
-    //! \brief Defines the source function.
-    void setSource(RealFunc* src) { source = src; }
+  //! \brief Defines the flux function.
+  void setFlux(RealFunc* f) { flux = f; }
 
-    //! \brief Defines the flux function.
-    void setFlux(RealFunc* f) { flux = f; }
+  void setFlux(VecFunc* vf) { vflux = vf; }
 
-    void setFlux(VecFunc* vf) { vflux = vf; }
+  //! \brief Evaluates the boundary fluid flux (if any) at specified point.
+  double getFlux(const Vec3& X, const Vec3& normal) const;
+  //! \brief Evaluates the potential source (if any) at specified point.
+  double getPotential(const Vec3& X) const;
 
-    //! \brief Evaluates the boundary fluid flux (if any) at specified point.
-    double getFlux(const Vec3& X, const Vec3& normal) const;
-    //! \brief Evaluates the potential source (if any) at specified point.
-    double getPotential(const Vec3& X) const;
+  //! \brief Returns a local integral contribution object for given element.
+  //! \param[in] nen Number of nodes on element
+  virtual LocalIntegral* getLocalIntegral(size_t nen, size_t,
+                                          bool neumann) const;
 
-    //! \brief Returns a local integral contribution object for given element.
-    //! \param[in] nen Number of nodes on element
-    virtual LocalIntegral* getLocalIntegral(size_t nen, size_t,
-                                            bool neumann) const;
+  //! \brief Returns a local integral container for the given element.
+  //! \param[in] nen Number of nodes on element
+  //! \param[in] neumann Whether or not we are assembling Neumann BC's
+  virtual bool evalInt(LocalIntegral& elmInt, const FiniteElement& fe,
+                       const Vec3& X) const;
 
-    //! \brief Returns a local integral container for the given element.
-    //! \param[in] nen Number of nodes on element
-    //! \param[in] neumann Whether or not we are assembling Neumann BC's
-    virtual bool evalInt(LocalIntegral& elmInt, const FiniteElement& fe,
-                         const Vec3& X) const;
+  //! \brief Evaluates the integrand at a boundary point.
+  //! \param elmInt The local integral object to receive the contributions
+  //! \param[in] fe Finite element data of current integration point
+  //! \param[in] X Cartesian coordinates of current integration point
+  //! \param[in] normal Boundary normal vector at current integration point
+  virtual bool evalBou(LocalIntegral& elmInt, const FiniteElement& fe,
+                       const Vec3& X, const Vec3& normal) const;
 
-    //! \brief Evaluates the integrand at a boundary point.
-    //! \param elmInt The local integral object to receive the contributions
-    //! \param[in] fe Finite element data of current integration point
-    //! \param[in] X Cartesian coordinates of current integration point
-    //! \param[in] normal Boundary normal vector at current integration point
-    virtual bool evalBou(LocalIntegral& elmInt, const FiniteElement& fe,
-                         const Vec3& X, const Vec3& normal) const;
+  //! \brief Sets up the permeability matrix
+  //! \param[out] K \f$ nsd\times nsd\f$-matrix or its inverse
+  //! \param[in] X Cartesian coordinates of current point
+  //! \param[in] invers If \e true, set up the inverse matrix instead
+  virtual bool formKmatrix(Matrix& K, const Vec3& X, bool inverse = false) const;
 
-    //! \brief Sets up the permeability matrix
-    //! \param[out] K \f$ nsd\times nsd\f$-matrix or its inverse
-    //! \param[in] X Cartesian coordinates of current point
-    //! \param[in] invers If \e true, set up the inverse matrix instead
-    virtual bool formKmatrix(Matrix& K, const Vec3& X, bool inverse = false) const;
+  //! \brief Evaluated the secondary solution at a result point
+  //! param[out] s Array of solution field values at current point
+  //! param[in] fe Finite element data at current point
+  //! param[in] X Cartesian coordinates of current point
+  //! param(in) MNPC Nodal point correspondence for the basis function values
+  virtual bool evalSol(Vector& s, const FiniteElement& fe,
+                       const Vec3& X, const std::vector<int>& MNPC) const;
 
-    //! \brief Evaluated the secondary solution at a result point
-    //! param[out] s Array of solution field values at current point
-    //! param[in] fe Finite element data at current point
-    //! param[in] X Cartesian coordinates of current point
-    //! param(in) MNPC Nodal point correspondence for the basis function values
-    virtual bool evalSol(Vector& s, const FiniteElement& fe,
-                         const Vec3& X, const std::vector<int>& MNPC) const;
+  //! \brief Evaluates the finite element (FE) solution at an integration point
+  //! \param[out] s The FE solution values at current point
+  //! \param[in] eV Element solution vector
+  //! \param[in] dNdX Basis function gradients at current point
+  //! \param[in] X Cartesian coordinates of current point
+  bool evalSol(Vector& s, const Vector& eV,
+               const Matrix& dNdX, const Vec3& X) const;
 
-    //! \brief Evaluates the finite element (FE) solution at an integration point
-    //! \param[out] s The FE solution values at current point
-    //! \param[in] eV Element solution vector
-    //! \param[in] dNdX Basis function gradients at current point
-    //! \param[in] X Cartesian coordinates of current point
-    bool evalSol(Vector& s, const Vector& eV,
-                 const Matrix& dNdX, const Vec3& X) const;
+  //! \brief Evaluates the analytical secondary solution at a result point.
+  //! \param[out] s The solution field values at current point
+  //! \param[in] asol The analytical solution field (tensor field)
+  //! \param[in] X Cartesian coordinates of current point
+  virtual bool evalSol(Vector& s, const VecFunc& asol, const Vec3& X) const;
 
-    //! \brief Evaluates the analytical secondary solution at a result point.
-    //! \param[out] s The solution field values at current point
-    //! \param[in] asol The analytical solution field (tensor field)
-    //! \param[in] X Cartesian coordinates of current point
-    virtual bool evalSol(Vector& s, const VecFunc& asol, const Vec3& X) const;
+  //! \brief Returns the number of primary/secondary solution field components.
+  //! \param[in] fld which field set to consider (1=primary, 2=secondary)
+  virtual size_t getNoFields(int fld = 2) const { return fld > 1 ? nsd : 1; }
 
-    //! \brief Returns the number of primary/secondary solution field components.
-    //! \param[in] fld which field set to consider (1=primary, 2=secondary)
-    virtual size_t getNoFields(int fld = 2) const { return fld > 1 ? nsd : 1; }
+  //! \brief Returns the name of the primary solution field.
+  //! \param[in] prefix Name prefix
+  virtual const char* getField1Name(size_t, const char* prefix = 0) const;
 
-    //! \brief Returns the name of the primary solution field.
-    //! \param[in] prefix Name prefix
-    virtual const char* getField1Name(size_t, const char* prefix = 0) const;
+  //! \brief Returns the name of a secondary solution field component
+  //! \param[in] i Field component index
+  //! \param[in] prefix Name prefix for all components
+  virtual const char* getField2Name(size_t i, const char* prefix = 0) const;
 
-    //! \brief Returns the name of a secondary solution field component
-    //! \param[in] i Field component index
-    //! \param[in] prefix Name prefix for all components
-    virtual const char* getField2Name(size_t i, const char* prefix = 0) const;
+  //! \brief Returns a pointer to an Integrand for solution norm evaluation.
+  //! \note The Integrand object is allocated dynamically and has to be deleted
+  //! manually when leaving the scope of the pointer variable receiving the
+  //! returned pointer value.
+  //! \param[in] asol Pointer to analytical solution (optional)
+  virtual NormBase* getNormIntegrand(AnaSol* asol = 0) const;
 
-    //! \brief Returns a pointer to an Integrand for solution norm evaluation.
-    //! \note The Integrand object is allocated dynamically and has to be deleted
-    //! manually when leaving the scope of the pointer variable receiving the
-    //! returned pointer value.
-    //! \param[in] asol Pointer to analytical solution (optional)
-    virtual NormBase* getNormIntegrand(AnaSol* asol = 0) const;
+private:
+  VecFunc* permvalues;    //!< Permeability function.
+  RealFunc* permeability; //!< Permeability field function.
 
-  private:
-    //Vec3 perm;
-    VecFunc* permvalues;
-    RealFunc* permeability;
+protected:
+  RealFunc* flux;         //!< Flux function.
+  RealFunc* source;       //!< Source function.
+  VecFunc* vflux;         //!< Flux function.
 
-  protected:
-
-    RealFunc* flux;
-    RealFunc* source;
-    VecFunc* vflux;
-
-  public:
-    unsigned short int nsd;
-    const double rhow;
-    const double gacc;
-    VecFunc* bodyforce;
+public:
+  unsigned short int nsd; //!< Number of spatial dimensions.
+  const double rhow;      //!< Density of fluid.
+  const double gacc;      //!< Gravity acceleration.
+  VecFunc* bodyforce;     //!< Body force function.
 };
 
+
+/*!
+ *   \brief Class representing the integrand of Darcy energy norms.
+ */
 
 class DarcyNorm : public NormBase
 {
--- SIMDarcy.h
+++ SIMDarcy.h
@@ -30,236 +30,249 @@
 
 template<class Dim> class SIMDarcy : public Dim
 {
-  public:
-    //! \brief Dummy declaration, no setup properties needed
-    typedef bool SetupProps;
+public:
+  //! \brief Dummy declaration, no setup properties needed
+  typedef bool SetupProps;
 
-    SIMDarcy() : Dim(1), DRC(Dim::dimension), solVec(&sol)
-    {
-      Dim::myProblem = &DRC;
-      aCode[0] = aCode[1] = 0;
+  //! \brief Default constructor.
+  SIMDarcy() : Dim(1), drc(Dim::dimension), solVec(&sol)
+  {
+    Dim::myProblem = &drc;
+    aCode[0] = aCode[1] = 0;
+  }
+
+  //! \brief Destructor.
+  virtual ~SIMDarcy()
+  {
+    Dim::myProblem = NULL;
+    Dim::myInts.clear();
+    // To prevent the SIMbase destructor try to delete already deleted functions
+    if (aCode[0] > 0) Dim::myScalars.erase(aCode[0]);
+    if (aCode[1] > 0) Dim::myVectors.erase(aCode[1]);
+  }
+
+  //! \brief Parses a data section from an XML element.
+  virtual bool parse(const TiXmlElement* elem)
+  {
+    if (strcasecmp(elem->Value(),"darcy"))
+      return this->Dim::parse(elem);
+
+    const TiXmlElement* child = elem->FirstChildElement();
+    for (; child; child = child->NextSiblingElement()) {
+      if (!strcasecmp(child->Value(),"permvalues")) {
+        const char* value = utl::getValue(child, "permvalues");
+        if (value) {
+          std::stringstream str;
+          str << value;
+          drc.setPermValues(new VecFuncExpr(value));
+        }
+      }
+      if (!strcasecmp(child->Value(),"permeability")){
+        std::string type;
+        utl::getAttribute(child, "type", type);
+        RealFunc* permeability = utl::parseRealFunc(utl::getValue(child, "permeability"), type);
+        std::cout << "permeability = " << permeability << std::endl;
+        drc.setPermField(permeability);
+      }
+      if (!strcasecmp(child->Value(),"bodyforce")) {
+        const char* value = utl::getValue(child, "bodyforce");
+        if (value) {
+          std::stringstream str;
+          str << value;
+          drc.setBodyForce(new VecFuncExpr(value));
+        }
+      }
+      if (!strcasecmp(child->Value(),"source")) {
+        std::string type;
+        utl::getAttribute(child,"type",type);
+        if (type == "Wavefront") {
+          std::cout << "Source function: Wavefront" << std::endl;
+          drc.setSource(new WavefrontSource());
+        }
+        else if (type == "expression" && child->FirstChild()) {
+          std::cout << "Source function: " << child->FirstChild()->Value() << std::endl;
+          drc.setSource(new EvalFunction(child->FirstChild()->Value()));
+        }
+      }
+      else if (!strcasecmp(child->Value(),"anasol")) {
+
+        std::string type;
+        utl::getAttribute(child,"type",type);
+        if (type == "Lshape") {
+          this->mySol = new AnaSol(new LshapeDarcy(), new LshapeDarcyVelocity());
+          std::cout << "Anasol: Lshape" << std::endl;
+        }
+        else if (type == "Wavefront") {
+          this->mySol = new AnaSol(new Wavefront(), new WavefrontVelocity());
+          std::cout << "Anasol: Wavefront" << std::endl;
+        } else {
+          this->mySol = new AnaSol(child);
+          std::cout << "Anasol: expression" << std::endl;
+        }
+        // Define the analytical boundary traction field
+        int code = 0;
+        if (code == 0 && utl::getAttribute(child,"code",code)) {
+          if (code > 0 && Dim::mySol->getScalarSecSol())
+          {
+            this->setPropertyType(code,Property::NEUMANN);
+            Dim::myVectors[code] = Dim::mySol->getScalarSecSol();
+            aCode[1] = code;
+          }
+        }
+      }
+      else
+        this->Dim::parse(child);
     }
+    return true;
+  }
 
-    virtual ~SIMDarcy()
+  //! \brief Initializes for integration of Neumann terms for a given property.
+  //! \param[in] propInd Physical property index
+  virtual bool initNeumann(size_t propInd)
+  {
+    typename Dim::SclFuncMap::const_iterator sit = Dim::myScalars.find(propInd);
+    typename Dim::VecFuncMap::const_iterator vit = Dim::myVectors.find(propInd);
+
+    if (sit != Dim::myScalars.end())
+      drc.setFlux(sit->second);
+    else if (vit != Dim::myVectors.end())
+      drc.setFlux(vit->second);
+    else
+      return false;
+
+    return true;
+  }
+
+  //! \brief Initializes the property containers of the model.
+  //! \details Use this method to clear the model before re-reading
+  //! the input file in the refinement step of an adaptive simulation.
+  virtual void clearProperties()
+  {
+    // To prevent SIMbase::clearProperties deleting the analytical solution
+    if (aCode[0] > 0) Dim::myScalars.erase(aCode[0]);
+    if (aCode[1] > 0) Dim::myVectors.erase(aCode[1]);
+    aCode[0] = aCode[1] = 0;
+
+    drc.setFlux((RealFunc*)NULL);
+    drc.setFlux((VecFunc*)NULL);
+    this->Dim::clearProperties();
+  }
+
+  //! \brief Returns the name of this simulator (for use in the HDF5 export).
+  virtual std::string getName() const
+  {
+    return "DarcyFlow";
+  }
+
+  //! \brief Set solution vector used.
+  //! \details Used to supply an external solution vector for adaptive simulations.
+  //! \param sol Pointer to vector to use.
+  void setSol(const Vector* sol) { solVec = sol; }
+
+  //! \brief Return solution vector.
+  const Vector& getSolution(int=0) { return *solVec; }
+
+  //! \brief Register fields for data export
+  void registerFields(DataExporter& exporter)
+  {
+    exporter.registerField("p,v", "primary", DataExporter::SIM,
+                           DataExporter::PRIMARY|DataExporter::SECONDARY);
+    exporter.setFieldValue("p,v", this, solVec);
+  }
+
+  //! \brief Opens a new VTF-file and writes the model geometry to it.
+  //! \param[in] fileName File name used to construct the VTF-file name from
+  //! \param[out] geoBlk Running geometry block counter
+  //! \param[out] nBlock Running result block counter
+  bool saveModel(char* fileName, int& geoBlk, int& nBlock)
+  {
+    if (Dim::opt.format < 0)
+      return true;
+
+    nBlock = 0;
+    return this->writeGlvG(geoBlk,fileName);
+  }
+
+  //! \brief Saves the converged results to VTF file of a given time step.
+  //! \param[in] tp Time step identifier
+  //! \param[in] nBlock Running VTF block counter
+  bool saveStep(const TimeStep& tp, int& nBlock)
+  {
+    if (Dim::opt.format < 0)
+      return true;
+
+    // Write solution fields
+    bool result = this->writeGlvS(*solVec, 1, nBlock, 0.0, 0);
+
+    return result && this->writeGlvStep(1, 0.0, 1);
+  }
+
+  //! \brief Computes the solution for the current time step.
+  bool solveStep(TimeStep& tp)
+  {
+    if (!this->assembleSystem())
+      return false;
+
+    if (!this->solveSystem(sol, Dim::msgLevel-1,"pressure    "))
+      return false;
+
+    tp.step--;
+    return true;
+  }
+
+  //! \brief Advances the time step one step forward.
+  bool advanceStep(TimeStep& tp) { return true; }
+
+protected:
+  //! \brief Performs some pre-processing tasks on the FE model.
+  //! \details This method is reimplemented to resolve inhomogeneous boundary
+  //! condition fields in case they are derived from the analytical solution.
+  virtual void preprocessA()
+  {
+    if (!Dim::mySol) return;
+
+    // Define analytical boundary condition fields
+    PropertyVec::iterator p;
+    for (p = Dim::myProps.begin(); p != Dim::myProps.end(); ++p)
     {
-      Dim::myProblem = NULL;
-      Dim::myInts.clear();
-      // To prevent the SIMbase destructor try to delete already deleted functions
-      if (aCode[0] > 0) Dim::myScalars.erase(aCode[0]);
-      if (aCode[1] > 0) Dim::myVectors.erase(aCode[1]);
-    }
-
-    virtual bool parse(const TiXmlElement* elem)
-    {
-      if (strcasecmp(elem->Value(),"darcy"))
-        return this->Dim::parse(elem);
-
-      const TiXmlElement* child = elem->FirstChildElement();
-      for (; child; child = child->NextSiblingElement()) {
-        if (!strcasecmp(child->Value(),"permvalues")) {
-          const char* value = utl::getValue(child, "permvalues");
-          if (value) {
-            std::stringstream str;
-            str << value;
-            DRC.setPermValues(new VecFuncExpr(value));
-          }
-        }
-        if (!strcasecmp(child->Value(),"permeability")){
-          std::string type;
-          utl::getAttribute(child, "type", type);
-          RealFunc* permeability = utl::parseRealFunc(utl::getValue(child, "permeability"), type);
-          std::cout << "permeability = " << permeability << std::endl;
-          DRC.setPermField(permeability);
-        }
-        if (!strcasecmp(child->Value(),"bodyforce")) {
-          const char* value = utl::getValue(child, "bodyforce");
-          if (value) {
-            std::stringstream str;
-            str << value;
-            DRC.setBodyForce(new VecFuncExpr(value));
-          }
-        }
-        if (!strcasecmp(child->Value(),"source")) {
-          std::string type;
-          utl::getAttribute(child,"type",type);
-          if (type == "Wavefront") {
-            std::cout << "Source function: Wavefront" << std::endl;
-            DRC.setSource(new WavefrontSource());
-          }
-          else if (type == "expression" && child->FirstChild()) {
-            std::cout << "Source function: " << child->FirstChild()->Value() << std::endl;
-            DRC.setSource(new EvalFunction(child->FirstChild()->Value()));
-          }
-        }
-        else if (!strcasecmp(child->Value(),"anasol")) {
-
-          std::string type;
-          utl::getAttribute(child,"type",type);
-          if (type == "Lshape") {
-            this->mySol = new AnaSol(new LshapeDarcy(), new LshapeDarcyVelocity());
-            std::cout << "Anasol: Lshape" << std::endl;
-          }
-          else if (type == "Wavefront") {
-            this->mySol = new AnaSol(new Wavefront(), new WavefrontVelocity());
-            std::cout << "Anasol: Wavefront" << std::endl;
-          } else {
-            this->mySol = new AnaSol(child);
-            std::cout << "Anasol: expression" << std::endl;
-          }
-          // Define the analytical boundary traction field
-          int code = 0;
-          if (code == 0 && utl::getAttribute(child,"code",code)) {
-            if (code > 0 && Dim::mySol->getScalarSecSol())
-            {
-              this->setPropertyType(code,Property::NEUMANN);
-              Dim::myVectors[code] = Dim::mySol->getScalarSecSol();
-              aCode[1] = code;
-            }
-          }
+      if (p->pcode == Property::DIRICHLET_ANASOL)
+      {
+        if (!Dim::mySol->getScalarSol())
+          p->pcode = Property::UNDEFINED;
+        else if (aCode[0] == abs(p->pindx))
+          p->pcode = Property::DIRICHLET_INHOM;
+        else if (aCode[0] == 0)
+        {
+          aCode[0] = abs(p->pindx);
+          Dim::myScalars[aCode[0]] = Dim::mySol->getScalarSol();
+          p->pcode = Property::DIRICHLET_INHOM;
         }
         else
-          this->Dim::parse(child);
+          p->pcode = Property::UNDEFINED;
       }
-      return true;
-    }
-
-    virtual bool initNeumann(size_t propInd)
-    {
-      typename Dim::SclFuncMap::const_iterator sit = Dim::myScalars.find(propInd);
-      typename Dim::VecFuncMap::const_iterator vit = Dim::myVectors.find(propInd);
-
-      if (sit != Dim::myScalars.end())
-        DRC.setFlux(sit->second);
-      else if (vit != Dim::myVectors.end())
-        DRC.setFlux(vit->second);
-      else
-        return false;
-
-      return true;
-    }
-
-    virtual void clearProperties()
-    {
-      // To prevent SIMbase::clearProperties deleting the analytical solution
-      if (aCode[0] > 0) Dim::myScalars.erase(aCode[0]);
-      if (aCode[1] > 0) Dim::myVectors.erase(aCode[1]);
-      aCode[0] = aCode[1] = 0;
-
-      DRC.setFlux((RealFunc*)NULL);
-      DRC.setFlux((VecFunc*)NULL);
-      this->Dim::clearProperties();
-    }
-
-    virtual std::string getName() const
-    {
-      return "DarcyFlow";
-    }
-
-    void setSol(const Vector* sol) { solVec = sol; }
-
-    const Vector& getSolution(int=0) { return *solVec; }
-
-    //! \brief Register fields for data export
-    void registerFields(DataExporter& exporter)
-    {
-      exporter.registerField("p,v", "primary", DataExporter::SIM,
-                             DataExporter::PRIMARY|DataExporter::SECONDARY);
-      exporter.setFieldValue("p,v", this, solVec);
-    }
-
-    //! \brief Opens a new VTF-file and writes the model geometry to it.
-    //! \param[in] fileName File name used to construct the VTF-file name from
-    //! \param[out] geoBlk Running geometry block counter
-    //! \param[out] nBlock Running result block counter
-    bool saveModel(char* fileName, int& geoBlk, int& nBlock)
-    {
-      if (Dim::opt.format < 0)
-        return true;
-
-      nBlock = 0;
-      return this->writeGlvG(geoBlk,fileName);
-    }
-
-    //! \brief Saves the converged results to VTF file of a given time step.
-    //! \param[in] tp Time step identifier
-    //! \param[in] nBlock Running VTF block counter
-    bool saveStep(const TimeStep& tp, int& nBlock)
-    {
-      if (Dim::opt.format < 0)
-        return true;
-
-      // Write solution fields
-      bool result = this->writeGlvS(*solVec, 1, nBlock, 0.0, 0);
-
-      return result && this->writeGlvStep(1, 0.0, 1);
-    }
-
-    //! \brief Computes the solution for the current time step.
-    bool solveStep(TimeStep& tp)
-    {
-      if (!this->assembleSystem())
-        return false;
-
-      if (!this->solveSystem(sol, Dim::msgLevel-1,"pressure    "))
-        return false;
-
-      tp.step--;
-      return true;
-    }
-
-    bool advanceStep(TimeStep& tp) { return true; }
-
-  protected:
-    //! \brief Performs some pre-processing tasks on the FE model.
-    //! \details This method is reimplemented to resolve inhomogeneous boundary
-    //! condition fields in case they are derived from the analytical solution.
-    virtual void preprocessA()
-    {
-      if (!Dim::mySol) return;
-
-      // Define analytical boundary condition fields
-      PropertyVec::iterator p;
-      for (p = Dim::myProps.begin(); p != Dim::myProps.end(); ++p)
+      else if (p->pcode == Property::NEUMANN_ANASOL)
       {
-        if (p->pcode == Property::DIRICHLET_ANASOL)
+        if (!Dim::mySol->getScalarSecSol())
+          p->pcode = Property::UNDEFINED;
+        else if (aCode[1] == p->pindx)
+          p->pcode = Property::NEUMANN;
+        else if (aCode[1] == 0)
         {
-          if (!Dim::mySol->getScalarSol())
-            p->pcode = Property::UNDEFINED;
-          else if (aCode[0] == abs(p->pindx))
-            p->pcode = Property::DIRICHLET_INHOM;
-          else if (aCode[0] == 0)
-          {
-            aCode[0] = abs(p->pindx);
-            Dim::myScalars[aCode[0]] = Dim::mySol->getScalarSol();
-            p->pcode = Property::DIRICHLET_INHOM;
-          }
-          else
-            p->pcode = Property::UNDEFINED;
+          aCode[1] = p->pindx;
+          Dim::myVectors[aCode[1]] = Dim::mySol->getScalarSecSol();
+          p->pcode = Property::NEUMANN;
         }
-        else if (p->pcode == Property::NEUMANN_ANASOL)
-        {
-          if (!Dim::mySol->getScalarSecSol())
-            p->pcode = Property::UNDEFINED;
-          else if (aCode[1] == p->pindx)
-            p->pcode = Property::NEUMANN;
-          else if (aCode[1] == 0)
-          {
-            aCode[1] = p->pindx;
-            Dim::myVectors[aCode[1]] = Dim::mySol->getScalarSecSol();
-            p->pcode = Property::NEUMANN;
-          }
-          else
-            p->pcode = Property::UNDEFINED;
-        }
+        else
+          p->pcode = Property::UNDEFINED;
       }
     }
+  }
 
-  private:
-    Darcy DRC;
-    Vectors pressure;
-    const Vector* solVec;
-    Vector sol;
-    int aCode[2];   //!< Analytical BC code (used by destructor)
+private:
+  Darcy drc;            //!< Darcy integrand.
+  const Vector* solVec; //!< Pointer to solution vector.
+  Vector sol;           //!< Internal solution vector.
+  int aCode[2];         //!< Analytical BC code (used by destructor)
 };
 
 
