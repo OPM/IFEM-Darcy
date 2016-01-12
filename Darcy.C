21d2d439b1444b08615c74095037a83f5cd2b093
Knut Morten Okstad <knut.morten.okstad@sintef.no>
2016-01-12 18:27:58 +0100

    Added: Default implementation of IntegrandBase::getNoFields(int group) returning the number of norm groups when the argument group is zero.

--- Darcy.C
+++ Darcy.C
@@ -374,14 +374,10 @@ void DarcyNorm::addBoundaryTerms (Vectors& gNorm, double energy) const
 
 size_t DarcyNorm::getNoFields (int group) const
 {
-  size_t nf = 1;
   if (group < 1)
-    for (size_t i = 0; i < prjsol.size(); i++)
-      nf += prjsol.empty() ? 0 : 1;
+    return this->NormBase::getNoFields();
   else
-    nf = anasol ? 4 : 2;
-
-  return nf;
+    return anasol ? 4 : 2;
 }
 
 
