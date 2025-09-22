9f0873178e40e5dbeb0ce1d7b40ad598c090decf
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2025-09-22 10:45:33 +0200

    convert unit-tests to catch

--- Common/Darcy/Test/TestSIMDarcy.C
+++ Common/Darcy/Test/TestSIMDarcy.C
@@ -17,32 +17,35 @@
 #include "SIM2D.h"
 #include "Vec3.h"
 
-#include <string>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 
-#include "gtest/gtest.h"
+using Catch::Matchers::WithinAbs;
+using Catch::Matchers::WithinRel;
 
-TEST(TestSIMDarcy, Parse)
+
+TEST_CASE("TestSIMDarcy.Parse")
 {
   Darcy itg(2);
   SIMDarcy<SIM2D> sim(itg);
-  EXPECT_TRUE(sim.read("Wavefront_k10_p2_b20.xinp"));
+  REQUIRE(sim.read("Wavefront_k10_p2_b20.xinp"));
 
   const Darcy& darcy = static_cast<const Darcy&>(*sim.getProblem());
 
   sim.init();
 
   Vec3 perm = darcy.getMaterial().getPermeability(Vec3());
-  ASSERT_FLOAT_EQ(perm[0], 98.1);
-  ASSERT_FLOAT_EQ(perm[1], 9.81);
-  ASSERT_FLOAT_EQ(perm[2], 0.0);
+  REQUIRE_THAT(perm[0], WithinRel(98.1));
+  REQUIRE_THAT(perm[1], WithinRel(9.81));
+  REQUIRE_THAT(perm[2], WithinAbs(0.0, 1e-14));
   Vec3 body = darcy.getBodyForce(Vec3());
-  ASSERT_FLOAT_EQ(body[0], 0.0);
-  ASSERT_FLOAT_EQ(body[1], 0.0);
-  ASSERT_FLOAT_EQ(body[2], 0.0);
+  REQUIRE_THAT(body[0], WithinAbs(0.0, 1e-14));
+  REQUIRE_THAT(body[1], WithinAbs(0.0, 1e-14));
+  REQUIRE_THAT(body[2], WithinAbs(0.0, 1e-14));
   double flux = darcy.getFlux(Vec3(),Vec3());
-  ASSERT_FLOAT_EQ(flux, 0.0);
+  REQUIRE_THAT(flux, WithinAbs(0.0, 1e-14));
   double src = darcy.getPotential(Vec3());
-  ASSERT_FLOAT_EQ(src, 1.5515174);
+  REQUIRE_THAT(src, WithinRel(1.5515174, 1e-7));
   src = darcy.getPotential(Vec3(0.25, 0.25, 0.0));
-  ASSERT_FLOAT_EQ(src, 156.157);
+  REQUIRE_THAT(src, WithinRel(156.157, 1e-6));
 }
