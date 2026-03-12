9bde36a9a82f10f781f6ac23ab28f4e702390eba
Knut Morten Okstad <knut.morten.okstad@sintef.no>
2026-03-12 16:48:57 +0100

    Added: Augmented Lagrange implementation

--- Common/Darcy/SIMDarcyTransportCorr.C
+++ Common/Darcy/SIMDarcyTransportCorr.C
@@ -13,6 +13,7 @@
 
 #include "SIMDarcyTransportCorr.h"
 
+#include "AlgEqSystem.h"
 #include "AnaSol.h"
 #include "ASMbase.h"
 #include "DataExporter.h"
@@ -52,6 +53,11 @@ SIMDarcyTransportCorr<Dim>::SIMDarcyTransportCorr (IntegrandBase& itg,
   useAL = AL;
   vCode = 0;
   nSclr = AL ? 3 : 0;
+  maxit = 10;
+  eps[0] = eps[1] = eps[2] = 1.0e99;
+
+  if (useAL)
+    Dim::msgLevel = 1;
 }
 
 
@@ -69,6 +75,11 @@ bool SIMDarcyTransportCorr<Dim>::parse (const tinyxml2::XMLElement* elem)
 {
   if (strcasecmp(elem->Value(),"darcycorrection"))
     return this->Dim::parse(elem);
+
+  utl::getAttribute(elem,"maxit",maxit);
+  utl::getAttribute(elem,"eps0",eps[0]);
+  utl::getAttribute(elem,"eps1",eps[1]);
+  utl::getAttribute(elem,"eps2",eps[2]);
 
   const tinyxml2::XMLElement* child = elem->FirstChildElement();
   for (; child; child = child->NextSiblingElement())
@@ -89,8 +100,11 @@ bool SIMDarcyTransportCorr<Dim>::parse (const tinyxml2::XMLElement* elem)
 template<class Dim>
 bool SIMDarcyTransportCorr<Dim>::preprocessBeforeAsmInit (int& nnod)
 {
-  if (this->mixedProblem())
+  if (!this->mixedProblem())
+    useAL = false; // Use penalty approach
+  else if (!useAL)
   {
+    // Use classical Lagrange multiplier approach with global constraints
     ++nnod;
     for (int p = 1; p <= this->getNoPatches(); ++p)
       if (int lp = this->getLocalPatchIndex(p); lp > 0)
@@ -166,7 +180,7 @@ bool SIMDarcyTransportCorr<Dim>::saveStep (const TimeStep& tp, int& nBlock)
 
 
 template<class Dim>
-bool SIMDarcyTransportCorr<Dim>::solveStep (const TimeStep& tp)
+bool SIMDarcyTransportCorr<Dim>::solveStep (TimeStep& tp)
 {
   if (Dim::msgLevel >= 0 && tp.multiSteps())
     IFEM::cout <<"\n  step = "<< tp.step
@@ -174,12 +188,62 @@ bool SIMDarcyTransportCorr<Dim>::solveStep (const TimeStep& tp)
 
   if (!this->setMode(SIM::DYNAMIC))
     return false;
+
   if (!this->initDirichlet())
     return false;
+
+  if (useAL)
+    return this->solveALStep(tp);
+
   if (!this->assembleSystem())
     return false;
 
   return this->solveSystem(qSol,Dim::msgLevel,"velocity");
+}
+
+
+template<class Dim>
+bool SIMDarcyTransportCorr<Dim>::solveALStep (TimeStep& tp)
+{
+  bool newLHS = true;
+  IFEM::cout << std::endl;
+
+  for (tp.iter = 0; tp.iter < maxit; tp.iter++)
+    if (tp.iter > 0 && this->checkConvergence(tp))
+      break;
+    else if (!this->assembleSystem(tp.time,{qSol},newLHS))
+      return false;
+    else if (!this->solveSystem(qSol))
+      return false;
+    else
+      newLHS = false;
+
+  this->printSolutionSummary(qSol,Dim::msgLevel,"velocity");
+  return true;
+}
+
+
+template<class Dim>
+bool SIMDarcyTransportCorr<Dim>::checkConvergence (TimeStep& tp)
+{
+  std::vector<double> rs(nSclr);
+  for (int i = 0; i < nSclr; i++)
+    rs[i] = sqrt(Dim::myEqSys->getScalar(i));
+
+  std::stringstream str;
+  if (Dim::adm.getProcId() == 0)
+  {
+    str <<"  iter="<< tp.iter-1;
+    for (size_t i = 1; i <= rs.size(); i++)
+      str <<" r"<< i <<"="<< rs[i-1];
+  }
+  IFEM::cout << str.str() << std::endl;
+
+  for (int i = 0; i < nSclr; i++)
+    if (rs[i] > eps[i])
+      return false;
+
+  return true;
 }
 
 
@@ -228,11 +292,10 @@ void SIMDarcyTransportCorr<Dim>::printSolutionSummary (const Vector&, int,
     double divq = 100.0*gNorm.front()[6]/gNorm.front()[5];
 
     if (Dim::adm.getProcId() == 0)
-      str << "\n  L2(q - q^) / L2(q^) = "<< diff
-          <<"%\n  L2(div q^) / L2(q^) = "<< divQ
-          <<"%\n  L2(res q^) / L2(c*q^) = "<< resQ
-          <<"%\n  L2(div q) / L2(q) = "<< divq
-          <<"% "<< gNorm.front()[6] <<" "<< gNorm.front()[5];
+      str <<"\n  L2(q - q^) / L2(q^)   = "<< diff <<"% of "<< gNorm.front()[0]
+          <<"\n  L2(div q^) / L2(q^)   = "<< divQ <<"% of "<< gNorm.front()[0]
+          <<"\n  L2(res q^) / L2(c*q^) = "<< resQ <<"% of "<< gNorm.front()[1]
+          <<"\n  L2(div q ) / L2(q )   = "<< divq <<"% of "<< gNorm.front()[5];
   }
 
   IFEM::cout << str.str() << std::endl;
